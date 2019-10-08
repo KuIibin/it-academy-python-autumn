253739044119aa08d0e8ab9f4cdaf1e36f4eb304
E.KyIibin <evgenii.kylibin@gmail.com>
2019-10-08 03:06:02 +0300

    Hometask 3.3

---
+++ Homework 3/Homework3-3.py
@@ -0,0 +1,16 @@
+# Посчитать количество строчных (маленьких) и прописных (больших) 
+# букв в введенной строке. Учитывать только английские буквы.
+
+sents = input('Введите предложение: ')
+sents = sents.replace(' ', '')
+upperCount = 0
+lowerCount = 0
+
+for i in sents:
+    if 'a' <= i <= 'z':
+        lowerCount += 1
+    elif 'A' <= i <= 'Z':
+        upperCount += 1
+
+print('Маленьких - {lowerCount} букв'.format(lowerCount=lowerCount))
+print('Больших - {upperCount} букв'.format(upperCount=upperCount))
