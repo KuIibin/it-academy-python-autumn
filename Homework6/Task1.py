c1aaf7c2fde363674681e18421651b13d8cb7519
E.KyIibin <evgenii.kylibin@gmail.com>
2019-11-03 21:52:38 +0300

    Task 1 done

--- Homework6/Task1.py
+++ Homework6/Task1.py
@@ -3,3 +3,16 @@
 #   runner() – все фукнции вызываются по очереди
 #   runner(‘gen_numbers’) – вызывается только функцию gen_numbers. 
 #   runner(‘func’, ‘func1’...) - вызывает все переданные функции 
+
+import AllTasksInFunc
+
+all_func = [el for el in dir(AllTasksInFunc) if not el.startswith('__')]
+
+def runner(*func_name):
+    if len(func_name) == 0:
+        func_name = all_func
+    for name in func_name:
+        func = getattr(AllTasksInFunc, name)
+        func()
+
+runner('is_palindrom')
