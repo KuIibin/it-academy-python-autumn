25b40c0b234f6c50095c105bcc82679b7bb9b742
E.KyIibin <evgenii.kylibin@gmail.com>
2019-11-03 10:11:58 +0300

    Task 3 update

--- Homework5/Task3.py
+++ Homework5/Task3.py
@@ -3,15 +3,18 @@
 # укажите, в какой стране он находится.
 
 number_contries = int(input('Введите колличество стран: '))
+
 countries_and_cityes = {}
 for el in range(number_contries):
     country, *city = input('Введите Страну и ее город: ').split()
     for elem in city:
         countries_and_cityes[elem] = country
 print('Словарь из Стран и их городов: ', countries_and_cityes)
+
 number_cityes = int(input('Введите к-во городов для проверки: '))
 cityes = []
 print('Вводите название городов, которые хотите проверить: ')
+
 for el in range(number_cityes):
     cityes.append(input())
 for el in cityes:
