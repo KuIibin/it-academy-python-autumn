07916b8493e7a455d92cf66db5237e2abb80a9b0
E.KyIibin <evgenii.kylibin@gmail.com>
2019-11-09 00:45:23 +0300

    Task 2 done

--- Homework6/Task2.py
+++ Homework6/Task2.py
@@ -2,3 +2,20 @@
 # результаты вызовы функций (за все время,
 # не только текущий запуск программы)
 
+def dec(sqrt):
+    def wrapper(x):
+        result = sqrt(x)
+        print(result)
+        f = open('Homework6/t2-results.txt', 'a')
+        f.write('Func "sqrt" has result: {}\n'.format(result))
+        f.close()
+    return wrapper
+
+
+@dec
+def sqrt(x):
+    return int(x ** 0.5)
+
+sqrt(100)
+sqrt(400)
+sqrt(900)
